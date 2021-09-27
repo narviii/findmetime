c2479518b9a3062dd7fef5b7074f9c8b5609738c
Vladimir Sofronov <narviii@gmail.com>
2021-09-26 17:57:03 -0700

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,35 @@
 import 'tailwindcss/tailwind.css'
 import '../css/extend.css'
-import { getAuth, signInAnonymously,updateProfile } from "firebase/auth";
+import { getAuth, signInAnonymously, updateProfile } from "firebase/auth";
 import { get, getDatabase, ref, child, set } from '@firebase/database';
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from './/..//helpers/firebase'
 const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
+import { useRouter } from 'next/router'
 
 
 
 
 async function signIn() {
+  const router = useRouter()
 
   const db = getDatabase();
   const auth = getAuth();
-  
-  try{
+
+  try {
     const user = (await signInAnonymously(auth)).user
-    if (user.displayName===null){
-      await updateProfile(auth.currentUser,{displayName: uniqueNamesGenerator({ dictionaries: [adjectives, animals], separator: ' ' })})
-    } 
-  }catch{
+    if (user.displayName === null) {
+      updateProfile(auth.currentUser, { displayName: uniqueNamesGenerator({ dictionaries: [adjectives, animals], separator: ' ' }) }).then(
+        () => {
+          update(ref(db, "sessions/" + router.query.id + "/users/" + auth.currentUser.uid), {
+            tz: moment.tz.guess(),
+            displayName: auth.currentUser.displayName
+          });
+        })
+
+
+    }
+  } catch {
     console.log("LOGIN ERROR")
   }
 }
