4c9f2fd16351c1a21e79fbacf0b223c5031af15b
narviii <narviii@gmail.com>
2021-09-29 01:13:28 -0700

    ...

--- components/timeline.js
+++ components/timeline.js
@@ -1,5 +1,5 @@
 var moment = require('moment-timezone');
-import React, {useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { SelectElement, ShowSelection } from './handle'
 import { DrawMarks } from './drawMarks'
 import { useRouter } from 'next/router'
@@ -11,8 +11,8 @@ import { Now } from '/components/now';
 import { useDrag } from '../hooks/useDrag'
 import { DrawDates } from './drawDates';
 import { onValue } from "@firebase/database";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { UserName } from './username';
+import { OutMark } from './outmark';
 
 
 export function ZoomTimeline({ children, control }) {
@@ -28,7 +28,7 @@ export function ZoomTimeline({ children, control }) {
 }
 
 
-export function TimeLinePassive({ control, isSelectedStart, tz, isSelectedEnd, timeLine }) {
+export function TimeLinePassive({ control, isSelectedStart, tz, isSelectedEnd, timeLine, uid }) {
     const handleMouseDown = useDrag(control, "translateTimeline")
     const isSelected = {
         start: isSelectedStart,
@@ -36,44 +36,30 @@ export function TimeLinePassive({ control, isSelectedStart, tz, isSelectedEnd, t
     }
     return (
         <React.Fragment>
-            <div className=" relative -translate-y-5 ">
-                <DrawDates timeLine={timeLine} tz={tz} />
-            </div>
-            <div onMouseDown={handleMouseDown} className={timeLineClass}>
-                <DrawMarks isSelected={isSelected} timeLine={timeLine} tz={tz} />
-                <ShowSelection isSelectedStart={isSelectedStart} isSelectedEnd={isSelectedEnd} timeLine={timeLine} />
-                <Now timeLine={timeLine} />
-                <OutMark timeLine={timeLine} isSelected={isSelected} />
-
+            <div className="grid grid-cols-11">
+                <div className="col-span-3">
+                    <div className=" relative h-4 overflow-hidden  ">
+                        
+                    </div>
+                    <UserName uid={uid} tz={tz} />
+                </div>
+                <div className="col-span-8">
+                    <div className=" relative h-4 overflow-hidden  ">
+                        <DrawDates timeLine={timeLine} tz={tz} />
+                    </div>
+                    <div onMouseDown={handleMouseDown} className={timeLineClass}>
+                        <DrawMarks isSelected={isSelected} timeLine={timeLine} tz={tz} />
+                        <ShowSelection isSelectedStart={isSelectedStart} isSelectedEnd={isSelectedEnd} timeLine={timeLine} />
+                        <Now timeLine={timeLine} />
+                        <OutMark timeLine={timeLine} isSelected={isSelected} />
+                    </div>
+                </div>
             </div>
 
         </React.Fragment>
     )
 }
 
-function OutMark({ timeLine, isSelected }) {
-
-    if (!isSelected.start) return null
-
-    if (isSelected.start.clone().isAfter(timeLine.end.clone())) {
-        return (
-            <div className={"  absolute  z-50 h-12 right-0   text-red-500"}>
-                <FontAwesomeIcon icon={faArrowRight} className="h-full" />
-            </div>
-        )
-        position = " right-0 "
-    } else if (isSelected.end.clone().isBefore(timeLine.start.clone())) {
-        return (
-            <div className=" absolute  z-50 h-12 left-0 text-red-500">
-                <FontAwesomeIcon icon={faArrowLeft} className="h-full" />
-            </div>
-        )
-    }
-
-    return null
-
-
-}
 
 
 
@@ -81,21 +67,22 @@ function OutMark({ timeLine, isSelected }) {
 
 
 
-export function TimelineActive({ control, isSelected, timeLine,setSelected }) {
+export function TimelineActive({ control, isSelected, timeLine, setZoomTimeline }) {
     const router = useRouter()
     const db = getDatabase();
     const user = useCurrentUser()
-    const handleMouseDown = useDrag(control, "setSelection",timeLine)
-    
-    
+    const handleMouseDown = useDrag(control, "setSelection", timeLine)
+    const timelineContainerRef = useRef(0)
+
+
     useEffect(() => {
         if (user) {
             onValue(ref(db, "sessions/" + router.query.id + "/users/" + user.uid), (snapshot) => {
                 const data = snapshot.val();
-                
-                if (data&&data.start && data.end) {
-                    
-                    setSelected({
+
+                if (data && data.start && data.end) {
+
+                    control({
                         type: "set",
                         timeline: timeLine,
                         start: moment(data.start),
@@ -107,51 +94,66 @@ export function TimelineActive({ control, isSelected, timeLine,setSelected }) {
             });
         }
     }, [user])
-    
-/*
+
+    useEffect(() => {
+        setZoomTimeline({ type: 'set_width', width: timelineContainerRef.current ? timelineContainerRef.current.offsetWidth : 0 })
+        setZoomTimeline({ type: 'set_domRect', domRect: timelineContainerRef.current ? timelineContainerRef.current.getBoundingClientRect() : 0 })
+
+
+        window.addEventListener('resize', function () {
+            setZoomTimeline({ type: 'set_width', width: timelineContainerRef.current ? timelineContainerRef.current.offsetWidth : 0 })
+            setZoomTimeline({ type: 'set_domRect', domRect: timelineContainerRef.current ? timelineContainerRef.current.getBoundingClientRect() : 0 })
+
+        });
+
+        return window.removeEventListener('resize', function () {
+            setZoomTimeline({ type: 'set_width', width: timelineContainerRef.current ? timelineContainerRef.current.offsetWidth : 0 })
+            setZoomTimeline({ type: 'set_domRect', domRect: timelineContainerRef.current ? timelineContainerRef.current.getBoundingClientRect() : 0 })
+
+        });
+    }, [])
+
+
+
     useEffect(async () => {
-        console.log(user.displayName)
-        if (user) {
+        if (user && (isSelected.start && isSelected.end)) {
             update(ref(db, "sessions/" + router.query.id + "/users/" + user.uid), {
+                start: isSelected.start.clone().utc().format(),
+                end: isSelected.end.clone().utc().format(),
                 tz: moment.tz.guess(),
                 displayName: user.displayName
             });
-            
-            
-           
         }
 
-    }, [user])
-    */
-    
-     useEffect(async () => {
-         if (user&&(isSelected.start&&isSelected.end)) {
-             update(ref(db, "sessions/" + router.query.id + "/users/" + user.uid), {
-                 start: isSelected.start.clone().utc().format(),
-                 end: isSelected.end.clone().utc().format(),
-                 tz: moment.tz.guess(),
-                 displayName: user.displayName
-             });
-         }
- 
-     }, [isSelected, user])
-     
+    }, [isSelected, user])
 
-    
+
+
 
     return (
 
         <React.Fragment>
-            <div className=" relative -translate-y-5  ">
-                <DrawDates timeLine={timeLine} />
-            </div>
-            <div onMouseDown={handleMouseDown} className={timeLineClass}>
-                <DrawMarks isSelected={isSelected} timeLine={timeLine} />
-                <SelectElement control={control} isSelected={isSelected} timeLine={timeLine} />
-                <Now timeLine={timeLine} scale={1} />
+            <div className="grid grid-cols-11">
 
-                <OutMark timeLine={timeLine} isSelected={isSelected} />
+                <div className="col-span-3">
+                    <div className=" h-4">
+
+                    </div>
+                    {user ? <UserName uid={user.uid} tz={user.tz} /> : null}
+                </div>
+                <div className="col-span-8 ">
+                    <div className=" relative h-4 overflow-hidden  ">
+                        <DrawDates timeLine={timeLine} />
+                    </div>
+                    <div ref={timelineContainerRef} onMouseDown={handleMouseDown} className={timeLineClass}>
+                        <DrawMarks isSelected={isSelected} timeLine={timeLine} />
+                        <SelectElement control={control} isSelected={isSelected} timeLine={timeLine} />
+                        <Now timeLine={timeLine} scale={1} />
+                        <OutMark timeLine={timeLine} isSelected={isSelected} />
+                    </div>
+                </div>
             </div>
+
         </React.Fragment>
 
     )
