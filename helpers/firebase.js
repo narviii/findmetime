6fb82f25f6bdc9e79963a0bc0fb4f57dbdc3ac16
Vladimir Sofronov <narviii@gmail.com>
2022-01-14 02:25:33 -0800

    housekeeping

--- helpers/firebase.js
+++ helpers/firebase.js
@@ -11,7 +11,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5HXjlxihufsP9G8r7IgmYvkN8XqGyuNk",
+  apiKey: process.env.REACT_APP_APIKEY,
   authDomain: "findmetime-928be.firebaseapp.com",
   projectId: "findmetime-928be",
   storageBucket: "findmetime-928be.appspot.com",
